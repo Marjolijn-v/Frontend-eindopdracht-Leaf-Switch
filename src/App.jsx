eae9ac09f41349c57a8d74f36bc225d1f624ae51
Marjolijn-v <mverspiek@gmail.com>
2026-01-22 17:04:03 +0100

    Routes toegevoegd en afbeeldingen in assets geplaatst

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route} from "react-router-dom";
+import NavBar from "./components/navBar/navBar.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import MyAccount from "./pages/MyAccount/MyAccount.jsx";
+import Register from "./pages/Register/Register.jsx";
+import Search from "./pages/Search/Search.jsx";
+import PlantDetails from "./pages/PlantDetails/PlantDetails.jsx";
 
 
 function App() {
@@ -7,10 +15,17 @@ function App() {
 
   return (
     <>
-
-
-
-
+        <NavBar />
+        <div className="content">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/account" element={<MyAccount />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/plant" element={<PlantDetails />} />
+            </Routes>
+        </div>
     </>
   )
 }
